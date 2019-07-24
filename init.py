deceb874ca1abc5cb4b18110b7d0b0fece81802f
ayan59dutta <ayan59dutta@gmail.com>
2019-07-25 02:17:42 +0530

    Create init.py
    Added support for multiple students.

---
+++ init.py
@@ -0,0 +1,36 @@
+from result_extractor import extract_results
+
+path = 'C:/makaut_result_extractor/'
+
+def extract_all(details):
+
+    changed = False
+    new_details = details[:]
+
+    for row_no, student in enumerate(details):
+        flag = student[-1]
+        if student[0] != '#' and flag == 'True':
+            roll_no = student[0]
+            semester_no = student[1]
+            email_id = student[2]
+            
+            if extract_results(roll_no, semester_no):
+                desktop_notifier(roll_no, semester_no)
+                send_email(roll_no, semester_no, email_id)
+                new_details[row_no][-1] = 'False'
+                changed = True
+
+    return (changed, new_details)
+
+
+if __name__ == '__main__':
+
+    with open(path+'details.txt') as fp:
+        details = list(map(lambda x: x.strip(' \n').split(), fp))
+
+    changed, new_details = extract_all(details)
+
+    if changed:
+        with open(path+'details.txt', 'w') as fp:
+            fp.write('\n'.join([' '.join(row) for row in new_details]))
+            
